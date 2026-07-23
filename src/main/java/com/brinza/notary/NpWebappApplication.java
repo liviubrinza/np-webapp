6af0a00c13c30c91650525f601c39ece375553e6
Liviu Flavius Brinza <liviu.brinza@mtrail.ch>
2026-07-23 14:06:26 +0300

    Initial project setup

---
+++ src/main/java/com/brinza/notary/NpWebappApplication.java
@@ -0,0 +1,13 @@
+package com.brinza.notary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NpWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NpWebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brinza/notary/NpWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brinza.notary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NpWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
