e81a949276032426d7168ed877bf694d4f482bf2
Liviu Flavius Brinza <liviu.brinza@mtrail.ch>
2026-08-10 11:54:25 +0300

    Fix

--- src/main/java/com/brinza/notary/config/WebConfig.java
+++ src/main/java/com/brinza/notary/config/WebConfig.java
@@ -2,10 +2,14 @@ package com.brinza.notary.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.time.Duration;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -20,5 +24,16 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addRedirectViewController("/", "/" + PathLocaleResolver.DEFAULT_LOCALE.getLanguage())
                 .setStatusCode(HttpStatus.MOVED_PERMANENTLY);
         registry.addViewController("/admin/login").setViewName("admin/login");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        CacheControl cacheControl = CacheControl.maxAge(Duration.ofDays(7)).cachePublic();
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("classpath:/static/images/")
+                .setCacheControl(cacheControl);
+        registry.addResourceHandler("/css/**")
+                .addResourceLocations("classpath:/static/css/")
+                .setCacheControl(cacheControl);
     }
 }
